50b209a16cceda89994ff14d36a05587776cfbc4
kinokinos <ee17093@yahoo.co.jp>
2016-03-31 11:11:58 +0900

    ans
    test

--- q1.py
+++ q1.py
@@ -1,1 +1,14 @@
-print "Hello"
+# -*- coding: utf-8 -*-
+
+print u"--2,8,10進数で回文，10以上で最小の値--"
+i = 11
+while True:
+	b = bin(i)
+	x = oct(i)
+	b = b.split('0b')[1]
+	x = x.split('0')[1]
+	if str(i) == str(i)[::-1] and b==b[::-1] and x==x[::-1] :
+		print "succeed"
+		print "ans="+str(i)
+		break
+	i+=1
