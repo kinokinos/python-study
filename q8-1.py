245f6c30e05038654a69b5cb98709081423edddf
ishikawa <ce56002@meiji.ac.jp>
2016-04-13 10:11:08 +0900

    q8-2

--- q8-1.py
+++ q8-1.py
@@ -3,27 +3,25 @@ import time
 print u"--経路探索．同じところを進まず，12ステップで何通り？--"
 start = time.time()
 import copy
+#traceの最後の配列が現在の位置
 
 state=[[0,-1],[0,1],[1,0],[-1,0]]
-now=[0,0]
 cont=0
-def search_path(trace,now,time):
+def search_path(trace,time):
     if time ==12:
         global cont
         cont+=1
         return 
     for i in state:
-        next=[0,0]
-        next[0] = now[0] + i[0]
-        next[1] = now[1] + i[1]
+        next=[trace[-1][0] + i[0],trace[-1][1] + i[1]]
         if next in trace:
             continue
         else:
             trace2 = copy.deepcopy(trace)
             trace2.append(next)
-            search_path(trace2,next,time+1)
+            search_path(trace2,time+1)
 
-search_path([[0,0]],now,0)
+search_path([[0,0]],0)
 print "count:"+str(cont)    
 
 elapsed_time = time.time() - start
