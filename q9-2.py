ca89396f044b0b2b99a1421b088563fb1747e42f
ishikawa <ce56002@meiji.ac.jp>
2016-04-18 13:06:46 +0900

    fix

--- q9-2.py
+++ q9-2.py
@@ -4,28 +4,21 @@ print u"--来た順，振り分け人数・男女を均等に--"
 start = time.time()
 import itertools
 cont=0
-l2=[]
-def create_list(s,b):
+def count_binary_permutations(s,b):
     global cont
-    global l2
     #print str(s.count("1"))+"::"+str(s.count("0"))
     if (b[0]==1 and b[1]==0) or (b[0]==0 and b[1]==1):
         cont+=1
-        l2.append(s)
         return
 
     if (s.count("1")>0 or s.count("0")>0) and (s.count("1")==s.count("0") or s.count("1")-10==s.count("0")):
-#        print s
-#        print str(s.count("1"))+"::"+str(s.count("0"))
         return
     if b[0]>0:
-        create_list(s+"0",[b[0]-1,b[1]])
+        count_binary_permutations(s+"0",[b[0]-1,b[1]])
     if b[1]>0:
-        create_list(s+"1",[b[0],b[1]-1])
+        count_binary_permutations(s+"1",[b[0],b[1]-1])
 
-create_list("",[10,20])
-#print l2
-print len(l2)
+count_binary_permutations("",[10,20])
 print cont
 
 elapsed_time = time.time() - start
